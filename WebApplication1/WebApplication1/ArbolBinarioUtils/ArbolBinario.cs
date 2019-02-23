5ff0e953279d55b3abede440fc2c7a64d12209c1
Willy Castillo <wcastillosalva@github.com>
2019-02-22 19:24:52 -0600

    creating branch

---
+++ WebApplication1/WebApplication1/ArbolBinarioUtils/ArbolBinario.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.ArbolBinarioUtils
+{
+    public class ArbolBinario<T>
+    {
+        private Nodo<T> Raiz;
+
+        public void Agregar(T valor, string nuevaLlave)
+        {
+            if(Raiz == null)
+            {
+                Nodo<T> nuevoNodo = new Nodo<T>();
+                nuevoNodo.Valor = valor;
+                nuevoNodo.Llave = nuevaLlave;
+                Raiz = nuevoNodo;
+            }
+            else {
+                Agregar(valor, nuevaLlave, Raiz);
+            }
+        }
+
+        private void Agregar(T valor, string nuevaLlave, Nodo<T> nodo)
+        {
+
+            Nodo<T> tmp = nodo;
+            //nuevaLlave < tmp.llave
+            if (nuevaLlave.CompareTo(tmp.Llave) == -1)
+            {
+                if (tmp.Izquierda == null)
+                {
+                    Nodo<T> nuevoNodo = new Nodo<T>();
+                    nuevoNodo.Valor = valor;
+                    nuevoNodo.Llave = nuevaLlave;
+                    tmp.Izquierda = nuevoNodo;
+                }
+                else
+                {
+                    Agregar(valor, nuevaLlave, tmp.Izquierda);
+                }
+            }
+            else
+            {
+                if (tmp.Derecha == null)
+                {
+                    Nodo<T> nuevoNodo = new Nodo<T>();
+                    nuevoNodo.Valor = valor;
+                    nuevoNodo.Llave = nuevaLlave;
+                    tmp.Derecha = nuevoNodo;
+                }
+                else
+                {
+                    Agregar(valor, nuevaLlave, tmp.Derecha);
+                }
+            }
+        }
+
+        public List<InfoIndice> Buscar(string valor)
+        {
+            List<InfoIndice> infoIndices = new List<InfoIndice>();
+            //TODO: recorer arbol y evaluar valor
+            return infoIndices;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Repository/ArbolBinarioRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.ArbolBinarioUtils;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Repository
+{
+    public class ArbolBinarioRepository
+    {
+        string path = @"C:\Users\wcast\git_lab05\ES12019\MOCK_DATA.csv";
+        ArbolBinario<InfoIndice> arbolBinario;
+
+        public ArbolBinarioRepository()
+        {
+            arbolBinario = new ArbolBinario<InfoIndice>();
+        }
+
+        public void LoadFile()
+        {
+            
+            System.IO.StreamReader lector = new System.IO.StreamReader(path);
+            while (!lector.EndOfStream)
+            {
+                string linea = lector.ReadLine();
+                string[] valores = linea.Split(",");
+                //numero de linea valores[0]
+                //llave nombre [1]
+                //cantidad[5]
+
+                InfoIndice nuevoIndice = new InfoIndice();
+                nuevoIndice.Linea = Convert.ToInt32(valores[0]);
+                nuevoIndice.Nombre = valores[1];
+                nuevoIndice.Existencia = Convert.ToInt32(valores[5]);
+                arbolBinario.Agregar(nuevoIndice, nuevoIndice.Nombre);
+            }
+            lector.Close();
+        }
+
+        public FarmacoEntity ObtenerFarmaco(int linea)
+        {
+            FarmacoEntity farmaco = new FarmacoEntity();
+            string line = File.ReadLines(path).Skip(linea+1).Take(1).First();
+            //TODO: Leer farmaco de archivo
+            return farmaco;
+
+        }
+
+        public List<FarmacoEntity> BuscarFarmacos(string valor, int numeroDePagina, int noElementos)
+        {
+            List<InfoIndice> listaIndices = arbolBinario.Buscar(valor).Skip(numeroDePagina -1 *noElementos).Take(noElementos).ToList();
+            List<FarmacoEntity> farmacoEntities = new List<FarmacoEntity>();
+            foreach (var item in listaIndices)
+            {
+                farmacoEntities.Add(ObtenerFarmaco(item.Linea));
+            }
+            return farmacoEntities;
+        }
+    }
+}
